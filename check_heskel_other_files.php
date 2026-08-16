4568f529eb5deb9055eb5470a0157430d3830d86
Porcalabs <porcalabs@gmail.com>
2026-08-16 10:43:23 +0700

    Clean up temporary check scripts

--- check_heskel_other_files.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dir = 'PAS_FOTO_SMPN_1_KEDUNGWARINGIN';
-$di = new RecursiveDirectoryIterator($dir);
-foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
-    if (stripos($filename, 'heskel') !== false || stripos($filename, 'arshavin') !== false) {
-        echo "$filename\n";
-    }
-}
-?>
